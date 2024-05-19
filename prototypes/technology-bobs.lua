de9856f0cd77363195e2dc5fac07c04948a0c631
KiwiHawk <59639+KiwiHawk@users.noreply.github.com>
2024-05-19 22:24:25 +1200

    Fixing compatibility issue

--- prototypes/technology-bobs.lua
+++ prototypes/technology-bobs.lua
@@ -1,98 +1,95 @@
 local bob_coefficient = 10
 local researchCost = settings.startup["SpaceX-research"].value
 if researchCost == nil then
-	researchCost = 1
+  researchCost = 1
 end
 
 local SpaceXTechs = {
-"space-assembly",
-"space-construction",
-"space-casings",
-"protection-fields",
-"fusion-reactor",
-"space-thrusters",
-"fuel-cells",
-"habitation",
-"life-support-systems",
-"spaceship-command",
-"astrometrics",
-"ftl-theory-A",
-"ftl-theory-B",
-"ftl-theory-C",
-"ftl-theory-D1",
-"ftl-theory-D2",
-"ftl-propulsion",
+  "space-assembly",
+  "space-construction",
+  "space-casings",
+  "protection-fields",
+  "fusion-reactor",
+  "space-thrusters",
+  "fuel-cells",
+  "habitation",
+  "life-support-systems",
+  "spaceship-command",
+  "astrometrics",
+  "ftl-theory-A",
+  "ftl-theory-B",
+  "ftl-theory-C",
+  "ftl-theory-D1",
+  "ftl-theory-D2",
+  "ftl-propulsion",
 }
 
-if data.raw.tool["advanced-logistic-science-pack"] then
+if bobmods.tech and bobmods.tech.advanced_logistic_science then
+  table.insert(SpaceXTechs, "ftl-theory-D")
 
-	table.insert(SpaceXTechs, "ftl-theory-D")
-
-    data:extend(
+  data:extend({
     {
-		{
-		type = "technology",
-		name = "ftl-theory-D",
-		icon = "__SpaceMod__/graphics/technology/ftl.png",	
-		icon_size = 128,
-		prerequisites = {"ftl-theory-C"},
-		unit =
-		{
-		count = 200000 * researchCost,
-		ingredients =
-		{
-			{"automation-science-pack", 1},
-			{"logistic-science-pack", 1},
-			{"chemical-science-pack", 1},
-			{"advanced-logistic-science-pack", 1},
-		},
-		time = 60
-		},
-		order = "k-o-a"
-	}
-	}
-	)
+      type = "technology",
+      name = "ftl-theory-D",
+      icon = "__SpaceMod__/graphics/technology/ftl.png",  
+      icon_size = 128,
+      prerequisites = {"ftl-theory-C"},
+      unit =
+        {
+          count = 200000 * researchCost,
+          ingredients =
+          {
+            {"automation-science-pack", 1},
+            {"logistic-science-pack", 1},
+            {"chemical-science-pack", 1},
+            {"advanced-logistic-science-pack", 1},
+          },
+          time = 60
+        },
+      order = "k-o-a"
+    }
+  })
 
-	bobmods.lib.tech.add_science_pack("space-assembly", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("space-construction", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("space-casings", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("protection-fields", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("fusion-reactor", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("space-thrusters", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("fuel-cells", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("habitation", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("life-support-systems", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("spaceship-command", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("astrometrics", "advanced-logistic-science-pack", 1)
-	bobmods.lib.tech.add_science_pack("ftl-propulsion", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("space-assembly", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("space-construction", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("space-casings", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("protection-fields", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("fusion-reactor", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("space-thrusters", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("fuel-cells", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("habitation", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("life-support-systems", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("spaceship-command", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("astrometrics", "advanced-logistic-science-pack", 1)
+  bobmods.lib.tech.add_science_pack("ftl-propulsion", "advanced-logistic-science-pack", 1)
 
 
-    data.raw.technology["ftl-theory-A"].unit.count = 200000 * researchCost
-    data.raw.technology["ftl-theory-B"].unit.count = 200000 * researchCost
-    data.raw.technology["ftl-theory-C"].unit.count = 200000 * researchCost
-	data.raw.technology["ftl-theory-D1"].unit.count = 200000 * researchCost
-	data.raw.technology["ftl-theory-D2"].unit.count = 200000 * researchCost
-    data.raw.technology["ftl-propulsion"].unit.count = 200000 * researchCost
+  data.raw.technology["ftl-theory-A"].unit.count = 200000 * researchCost
+  data.raw.technology["ftl-theory-B"].unit.count = 200000 * researchCost
+  data.raw.technology["ftl-theory-C"].unit.count = 200000 * researchCost
+  data.raw.technology["ftl-theory-D1"].unit.count = 200000 * researchCost
+  data.raw.technology["ftl-theory-D2"].unit.count = 200000 * researchCost
+  data.raw.technology["ftl-propulsion"].unit.count = 200000 * researchCost
 
-    bobmods.lib.tech.replace_prerequisite("ftl-theory-D1", "ftl-theory-C", "ftl-theory-D")
-    bobmods.lib.tech.replace_prerequisite("ftl-theory-D2", "ftl-theory-C", "ftl-theory-D")	
+  bobmods.lib.tech.replace_prerequisite("ftl-theory-D1", "ftl-theory-C", "ftl-theory-D")
+  bobmods.lib.tech.replace_prerequisite("ftl-theory-D2", "ftl-theory-C", "ftl-theory-D")  
 
 end
 
-bobmods.lib.tech.add_prerequisite("space-assembly", "bob-robots-3")	
+bobmods.lib.tech.add_prerequisite("space-assembly", "bob-robots-3")  
 
 if bobmods.modules.EnableGodModules == true then
-	bobmods.lib.tech.add_prerequisite("space-assembly", "god-module-5")
+  bobmods.lib.tech.add_prerequisite("space-assembly", "god-module-5")
 --    data.raw.technology["space-assembly"].prerequisites = {"god-module-5","rocket-silo","bob-robots-3"}
 else
-	bobmods.lib.tech.add_prerequisite("space-assembly", "speed-module-8")
-	bobmods.lib.tech.add_prerequisite("space-assembly", "effectivity-module-8")
+  bobmods.lib.tech.add_prerequisite("space-assembly", "speed-module-8")
+  bobmods.lib.tech.add_prerequisite("space-assembly", "effectivity-module-8")
 --    data.raw.technology["space-assembly"].prerequisites = {"speed-module-8","effectivity-module-8","rocket-silo","bob-robots-3"}
-	bobmods.lib.tech.add_prerequisite("ftl-propulsion", "productivity-module-8")
---    data.raw.technology["ftl-propulsion"].prerequisites = {"productivity-module-8","ftl-theory-D"}	
+  bobmods.lib.tech.add_prerequisite("ftl-propulsion", "productivity-module-8")
+--    data.raw.technology["ftl-propulsion"].prerequisites = {"productivity-module-8","ftl-theory-D"}  
 end
 
-bobmods.lib.tech.add_prerequisite("space-construction", "bob-robo-modular-4")	
+bobmods.lib.tech.add_prerequisite("space-construction", "bob-robo-modular-4")  
 -- data.raw.technology["space-construction"].prerequisites = {"space-assembly","bob-robo-modular-4"}
 bobmods.lib.tech.add_prerequisite("protection-fields", "bob-energy-shield-equipment-6")
 -- data.raw.technology["protection-fields"].prerequisites = {"space-construction","energy-shield-equipment-6"}
@@ -111,8 +108,8 @@ bobmods.lib.tech.add_recipe_unlock("protection-fields", "protection-field-goople
 
 -- Nerf Bob's end game
 for i, tech in pairs(SpaceXTechs) do
-	local rootTech = data.raw.technology[tech]
-	if rootTech ~= nil then
-		rootTech.unit.count = rootTech.unit.count * bob_coefficient
-	end
+  local rootTech = data.raw.technology[tech]
+  if rootTech ~= nil then
+    rootTech.unit.count = rootTech.unit.count * bob_coefficient
+  end
 end
